675e442506314edc9fbac082e2852672a408e529
John Markus Bjørndalen <jmb@cs.uit.no>
2022-03-15 01:34:32 +0100

    can now pull down info about assignments in old courses

--- get-submission-info.py
+++ get-submission-info.py
@@ -9,9 +9,15 @@ import json
 import os
 
 BASE_URL  = "https://uit.instructure.com/"   # Canvas for uit.no
+
+COURSE_ID = 4525                             # inf-1400 2018
+COURSE_ID = 11589                            # inf-1400 2019
 COURSE_ID = 16497                            # inf-1400 2020
 COURSE_ID = 21176                            # inf-1400 2021
 COURSE_ID = 24906                            # inf-1400 2022
+
+# To view submissions after a course is closed, or to view students that have withdrawn from the course.
+INCLUDE_COMPLETED = True
 
 api_key = open("api_key.txt", 'r').readline().strip()
 canvas = canvasapi.Canvas(BASE_URL, api_key)
@@ -19,9 +25,16 @@ course = canvas.get_course(COURSE_ID)
 
 
 def stud_to_dict(stud):
+    if hasattr(stud, 'display_name'):
+        name = stud.display_name
+    else:
+        # get_users returns users without display_name
+        name = stud.short_name
+
     return {
         'id' : stud.id,
-        'display_name' : stud.display_name,
+        'display_name' : name,
+        'is_completed' : stud.id in students_compl,
     }
 
 
@@ -38,11 +51,39 @@ def subm_to_dict(subm, studs):
     }
 
 
+def has_user_enrollment(enrollments):
+    "Any of the enrollments has a student role"
+    return any(e['type'] == 'StudentEnrollment' for e in enrollments)
+
+
+def is_completed_student(user):
+    "Has a student enrollment and is not a currently active user"
+    # Note that students with a "completed" status in get_users() can still be active as they
+    # may simply have switched over to a different group and are therefore registered as completed in the
+    # old group. This is why it is necessary to filter out the active users to avoid adding the students twice.
+    return has_user_enrollment(user.enrollments) and user.id not in students
+
+
+def get_compl_submissions(assignment):
+    """Submissions from completed students"""
+    return [assignment.get_submission(stud_id, include=['submission_history', 'submission_comments'])
+            for stud_id in students_compl]
+
+
 # Make sure the cache directory exists
 os.makedirs(".cache", exist_ok=True)
 print("Fetching assignments and students")
 assignments = list(course.get_assignments())
 assignments.sort(key=lambda x: x.name)
+# Active students
+students = {s.id : s for s in course.get_users(include=['enrollments']) if has_user_enrollment(s.enrollments)}
+print(f"  - {len(students)} active students")
+# Students that have withddrawn or are marked as concluded/completed/prior, which happens
+# to almost all students when the semester is over.
+students_compl = {s.id : s for s in course.get_users(enrollment_state=['completed'], include=['enrollments'])
+                  if is_completed_student(s)}
+print(f"  - {len(students_compl)} completed students")
+
 alist = []
 for a in assignments:
     print('Fetching info for', a.name)
@@ -50,6 +91,12 @@ for a in assignments:
     print(' -- got submissions')
     studs = list(a.get_gradeable_students())
     print(' -- got students')
+
+    if INCLUDE_COMPLETED:
+        studs.extend(students_compl.values())
+        print(" -- adding submissions from completed students")
+        subs += get_compl_submissions(a)
+
     f_studs = {int(s.id) : stud_to_dict(s) for s in studs}
     ad = {
         'created_at' : a.created_at,
